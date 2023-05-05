2026d457b3b6959c4dd76e1f566aeb68a34d82cb
MarinaPaley <marina_paley@mail.ru>
2023-05-05 23:13:45 +0300

    Ошибки в орфографии

--- Solver/RiemannSumRight.h
+++ Solver/RiemannSumRight.h
@@ -1,50 +1,53 @@
 #pragma once
 #include <functional>
 
-/**
-* @brief Расчет интеграла методом правых прямоугольников.
-*/
-class RiemannSumRight
+namespace
 {
-private:
     /**
-     * @brief Левая граница определенного интеграла.
+    * @brief Расчет интеграла методом правых прямоугольников.
     */
-    double start;
+    class RiemannSumRight
+    {
+    private:
+        /**
+         * @brief Левая граница определенного интеграла.
+        */
+        double start;
 
-    /**
-     * @brief Правая граница определенного интеграла.
-    */
-    double finish;
+        /**
+         * @brief Правая граница определенного интеграла.
+        */
+        double finish;
 
-    /**
-     * @brief Шаг численного расчета интеграла.
-    */
-    double step;
+        /**
+         * @brief Шаг численного расчета интеграла.
+        */
+        double step;
 
-    /**
-     * @brief Функция, которую небходимо проинтегрировать.
-    */
-    std::function<double(const double)> function;
+        /**
+         * @brief Функция, которую небходимо проинтегрировать.
+        */
+        std::function<double(const double)> function;
 
-public:
-    /**
-     * @brief Создает экземпляр класса "RiemannSumRight"
-     * @param start Левая граница определенного интеграла.
-     * @param finish Правая граница определенного интеграла.
-     * @param step Шаг численного расчета интеграла.
-     * @param function Функция, которую небходимо проинтегрировать.
-    */
-    RiemannSumRight(
-        const double start,
-        const double finish,
-        const double step,
-        const std::function<double(const double)> function);
+    public:
+        /**
+         * @brief Создает экземпляр класса "RiemannSumRight"
+         * @param start Левая граница определенного интеграла.
+         * @param finish Правая граница определенного интеграла.
+         * @param step Шаг численного расчета интеграла.
+         * @param function Функция, которую небходимо проинтегрировать.
+        */
+        RiemannSumRight(
+            const double start,
+            const double finish,
+            const double step,
+            const std::function<double(const double)> function);
 
-    /**
-     * @brief Расчет интеграла методом правых прямоугольников.
-     * @return Численное знаение интеграла.
-    */
-    double GetIntegral() const;
-};
+        /**
+         * @brief Расчет интеграла методом правых прямоугольников.
+         * @return Численное знаение интеграла.
+        */
+        double GetIntegral() const;
+    };
+}
 
